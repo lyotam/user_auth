c7f06456199b0dd0f53e6dcb5c4b4c0056ad148b
Yotam Leshem <leshemyotam@gmail.com>
2020-12-03 14:09:17 +0200

    Added users_controller index, show, create endpoints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :json => @users#, :include => tasks
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save    #TODO: CHANGE
+      render :json => @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end
