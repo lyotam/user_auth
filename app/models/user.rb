72074841a6a0c29d8e2bb5714e1244c43df16fb8
Yotam Leshem <leshemyotam@gmail.com>
2020-12-03 16:03:42 +0200

    Added validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  
+  validates :first_name, :last_name, :email, presence: true
+  validates :email,
+  format: { with: /\A(.+)@(.+)\z/, message: "Email invalid" },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 } 
+
 end
