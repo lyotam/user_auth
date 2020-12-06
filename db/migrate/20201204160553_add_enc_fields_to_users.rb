06f6009259628f485addefb20b48777e4712c0f5
Yotam Leshem <leshemyotam@gmail.com>
2020-12-06 18:53:50 +0200

    Added missing iv values

--- db/migrate/20201204160553_add_enc_fields_to_users.rb
+++ db/migrate/20201204160553_add_enc_fields_to_users.rb
@@ -1,6 +1,8 @@
 class AddEncFieldsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :encrypted_password, :string
+    add_column :users, :encrypted_password_iv, :string
     add_column :users, :encrypted_token, :string
+    add_column :users, :encrypted_token_iv, :string
   end
 end
